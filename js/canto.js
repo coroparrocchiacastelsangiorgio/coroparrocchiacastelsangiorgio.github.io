461e94da9b5e167c2cb6a683a8d96c58aa21d727
unknown <giuliasellitto.gs@gmail.com>
2022-05-15 20:23:04 +0200

    finito indice

---
+++ js/canto.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', mostracanto);
+
+function mostracanto() {
+/*
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.has('id')) {
+    var y = urlParams.get('year');
+    if (editions.includes(y)) {
+        return y;
+    }
+}
+<h1 id="titolo"></h1>
+    <div id="tag"></div>
+    <div id="testo"></div>*/
+}
--- js/indice.js
+++ js/indice.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', mostraindice);
 
-
 function mostraindice() {
     fetch('https://coroparrocchiacastelsangiorgio.github.io/db/indice.json')
         .then(response => response.text())
@@ -11,36 +10,53 @@ function mostraindice() {
                 return first.titolo.localeCompare(second.titolo);
             });
             // popola la lista canti
-            ul = document.querySelector("#lista-canti");
-            li = ul.querySelector('.elemento-lista-canti');
+            ul = document.getElementById("lista-canti");
+            li = ul.getElementsByClassName('elemento-lista-canti')[0];
             indicejson.forEach(cantojson => {
                 nuovonodo = li.cloneNode(true);
-                a = nuovonodo.querySelector(".link-lista-canti")
-                a.innerText = cantojson.titolo;
-                a.href = "db/testi/" + cantojson.id + ".html";
-                // TODO add tags in span elements with class name "tag-lista-canti"
+                a = nuovonodo.getElementsByClassName("link-lista-canti")[0];
+                a.textContent = cantojson.titolo;
+                a.href = "canto.html?id=" + cantojson.id;
+                // aggiungi tag in elementi span (saranno invisibili)
+                span = nuovonodo.getElementsByClassName("tag-lista-canti")[0];
+                tag = cantojson.tag;
+                tag.forEach(tagjson => {
+                    nuovospan = span.cloneNode(true);
+                    nuovospan.textContent = tagjson;
+                    nuovonodo.appendChild(nuovospan);
+                });
+                span.remove();
+                // aggiungi nuovo elemento alla lista canti
                 ul.appendChild(nuovonodo);
             });
+            li.remove();
         });
 }
 
 function ricerca() {
     // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.querySelector('#barra-ricerca-canti');
-    filter = input.value.toUpperCase();
-    ul = document.querySelector("#lista-canti");
-    li = ul.querySelector('.elemento-lista-canti');
-
+    var input, ul, li, a, spanlist, i, j, found;
+    input = document.getElementById('barra-ricerca-canti').value.toUpperCase();
+    ul = document.getElementById("lista-canti");
+    li = ul.getElementsByClassName('elemento-lista-canti');
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
-        a = li[i].querySelector(".link-lista-canti")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) <= -1) {
+        found = false;
+        a = li[i].getElementsByClassName("link-lista-canti")[0];
+        if (a.textContent.toUpperCase().indexOf(input) > -1) {
+            found = true;
+        }
+        spanlist = li[i].getElementsByClassName("tag-lista-canti");
+        for (j = 0; j < spanlist.length; j++) {
+            if (spanlist[j].textContent.toUpperCase().indexOf(input) > -1) {
+                found = true;
+            }
+        }
+        if (found == true) {
+            li[i].style.display = "";
+        }
+        else {
             li[i].style.display = "none";
         }
     }
-
-    // TODO loop through span elements with class name "tag-lista-canti" -- each li has several spans
-
 }
