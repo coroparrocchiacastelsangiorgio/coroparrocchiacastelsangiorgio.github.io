5404ab4a62022d5c24c4ae3644e82a15259eb10b
unknown <giuliasellitto.gs@gmail.com>
2022-05-15 16:22:09 +0200

    indice

--- js/indice.js
+++ js/indice.js
@@ -2,31 +2,46 @@ document.addEventListener('DOMContentLoaded', mostraindice);
 
 
 function mostraindice() {
-    fetch('db/indice.json')
+    //fetch('db/indice.json')
+    fetch("https://raw.githubusercontent.com/coroparrocchiacastelsangiorgio/coroparrocchiacastelsangiorgio.github.io/main/db/indice.json")
         .then(response => response.text())
         .then(data => {
-            // Do something with your data
-            // indice.json è una lista. ordina per titolo, dopodiche mostra nella lista con id "lista-canti"
-            console.log(data);
+            indicejson = JSON.parse(data);
+            // ordina per titolo
+            indicejson.sort(function (first, second) {
+                return first.titolo.localeCompare(second.titolo);
+            });
+            // popola la lista canti
+            ul = document.querySelector("#lista-canti");
+            li = ul.querySelector('.elemento-lista-canti');
+            indicejson.forEach(cantojson => {
+                nuovonodo = li.cloneNode(true);
+                a = nuovonodo.querySelector(".link-lista-canti")
+                a.innerText = cantojson.titolo;
+                a.href = "db/testi/" + cantojson.id + ".html";
+                // TODO add tags in span elements with class name "tag-lista-canti"
+                ul.appendChild(nuovonodo);
+            });
         });
 }
 
 function ricerca() {
     // Declare variables
     var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('barra-ricerca-canti');
+    input = document.querySelector('#barra-ricerca-canti');
     filter = input.value.toUpperCase();
-    ul = document.getElementById("lista-canti");
-    li = ul.getElementsByTagName('li');
+    ul = document.querySelector("#lista-canti");
+    li = ul.querySelector('.elemento-lista-canti');
 
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
+        a = li[i].querySelector(".link-lista-canti")[0];
         txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
+        if (txtValue.toUpperCase().indexOf(filter) <= -1) {
             li[i].style.display = "none";
         }
     }
+
+    // TODO loop through span elements with class name "tag-lista-canti" -- each li has several spans
+
 }
