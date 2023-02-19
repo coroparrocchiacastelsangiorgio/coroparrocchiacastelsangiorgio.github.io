59f6c0ed1599c21efe5d8991bcad40a29da1d7c0
Giulia Sellitto <giuliasellitto.gs@gmail.com>
2023-02-19 19:38:42 +0100

    fix deploy

--- js/oggi.js
+++ js/oggi.js
@@ -1,61 +1,48 @@
 document.addEventListener('DOMContentLoaded', caricamessadelgiorno);
 
 function caricamessadelgiorno() {
-
     fetch('https://coroparrocchiacastelsangiorgio.github.io/oggi.json')
-        .then(response => response.text())
-        .then(data => {
-            datamessa = JSON.parse(data);
+        .then(response1 => response1.text())
+        .then(data1 => {
+            datamessa = JSON.parse(data1);
+            console.log(datamessa);
             fetch("https://coroparrocchiacastelsangiorgio.github.io/db/storico-messe/" + datamessa + ".json")
-                .then(response => response.text())
-                .then(data => {
-                    oggi = JSON.parse(data);
-                    oggiparentdiv = document.getElementsByClassName("oggi-parent-div")[0];
-                    mainoggidiv = oggiparentdiv.getElementsByClassName("main-oggi-div")[0];
-                    oggi.forEach(messa => {
-                        newoggidiv = mainoggidiv.cloneNode(true);
-                        newoggidiv.getElementsByClassName("oggi-data-h3")[0].textContent = messa.datamessa;
-                        newoggidiv.getElementsByClassName("oggi-nome-h3")[0].textContent = messa.nome;
-                        newoggidiv.getElementsByClassName("oggi-sottotitolo-h3")[0].textContent = messa.sottotitolo;
-                        newoggidiv.getElementsByClassName("oggi-liturgia-a")[0].textContent += formatdatamessaperliturgia(datamessa);
-                        // popola scaletta
-                        maincantodiv = newoggidiv.getElementsByClassName("oggi-canto-div")[0];
-                        messa.scaletta.forEach(elementoscaletta => {
-                            newcantodiv = maincantodiv.cloneNode(true);
-                            newcantodiv.getElementsByClassName("oggi-canto-momento-h4")[0].textContent = elementoscaletta.momento;
-                            if ("idcanto" in elementoscaletta) {
-                                // TODO fetch canto sulla base di id
-
-                                fetch('https://coroparrocchiacastelsangiorgio.github.io/db/indice.json')
-                                    .then(response => response.text())
-                                    .then(data => {
-                                        indicejson = JSON.parse(data);
-                                        // cerca il canto nell'indice
-                                        metadatijson = indicejson.find(canto => {
-                                            return canto.id == elementoscaletta.idcanto;
-                                        });
-                                        titolocanto = metadatijson.titolo;
-                                    });
-
-                                fetch('https://coroparrocchiacastelsangiorgio.github.io/db/testi/' + elementoscaletta.idcanto + '.html')
-                                    .then(response => response.text())
-                                    .then(data => {
-                                        // popola il testo
-                                        testocanto = data;
-                                    });
-
-                                newcantodiv.getElementsByClassName("oggi-canto-testo-p")[0].textContent = testocanto;
-                                newcantodiv.getElementsByClassName("oggi-canto-titolo-h4")[0].textContent = titolocanto;
+                .then(response2 => response2.text())
+                .then(data2 => {
+                    elencomesse = JSON.parse(data2);
+                    divmesse = document.querySelector(".messe-div");
+                    divmessa = divmesse.querySelector(".messa-div");
+                    for (i = 1; i < elencomesse.length; i++) {
+                        nuovodiv = divmessa.cloneNode(true);
+                        divmesse.appendChild(nuovodiv);
+                    }
+                    for (i = 0; i < elencomesse.length; i++) {
+                        divmessa = divmesse.querySelectorAll(".messa-div")[i];
+                        messa = elencomesse[i];
+                        divmessa.querySelector(".data-messa").textContent = messa.datamessa;
+                        divmessa.querySelector(".nome-messa").textContent = messa.nome;
+                        divmessa.querySelector(".sottotitolo-messa").textContent = messa.sottotitolo;
+                        divmessa.querySelector(".link-liturgia").href += formatdatamessaperliturgia(datamessa);
+                        divmomenti = divmessa.querySelector(".momenti");
+                        divmomento = divmomenti.querySelector(".momento-div");
+                        for (j = 1; j < messa.scaletta.length; j++) {
+                            nuovomomento = divmomento.cloneNode(true);
+                            divmomenti.appendChild(nuovomomento);
+                        }
+                        for (j = 0; j < messa.scaletta.length; j++) {
+                            infomomento = messa.scaletta[j];
+                            divmomento = divmomenti.querySelectorAll(".momento-div")[j];
+                            divmomento.querySelector(".nome-momento").textContent = infomomento.momento;
+                            if ("idcanto" in infomomento) {
+                                console.log(infomomento.idcanto);
+                                popolatitolocanto(i, j, infomomento.idcanto);
+                                popolatestocanto(i, j, infomomento.idcanto);
                             }
-                            if ("testocanto" in elementoscaletta) {
-                                newcantodiv.getElementsByClassName("oggi-canto-testo-p")[0].textContent = testocanto;
+                            if ("testocanto" in infomomento) {
+                                divmomento.querySelector(".testo-canto").innerHTML = infomomento.testocanto;
                             }
-                            newoggidiv.appendChild(newcantodiv);
-                        });
-                        maincantodiv.remove();
-                        oggiparentdiv.appendChild(newoggidiv);
-                    });
-                    mainoggidiv.remove();
+                        }
+                    }
                 });
         });
 }
@@ -67,3 +54,38 @@ function formatdatamessaperliturgia(datamessa) {
     let day = splitted[2];
     return year + month + day;
 }
+
+function popolatitolocanto(imessa, jcanto, idcanto) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', "https://coroparrocchiacastelsangiorgio.github.io/db/indice.json", true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        var status = xhr.status;
+        if (status == 200) {
+            indicejson = xhr.response;
+            // cerca il canto nell'indice
+            metadatijson = indicejson.find(canto => {
+                return canto.id == idcanto;
+            });
+            console.log(metadatijson);
+            dapopolare = document.querySelectorAll(".messa-div")[imessa].querySelectorAll(".momento-div")[jcanto];
+            dapopolare.querySelector(".titolo-canto").textContent = metadatijson.titolo;
+        }
+    };
+    xhr.send();
+}
+
+function popolatestocanto(imessa, jcanto, idcanto) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://coroparrocchiacastelsangiorgio.github.io/db/testi/' + idcanto + '.html', true);
+    xhr.responseType = 'text';
+    xhr.onload = function() {
+        var status = xhr.status;
+        if (status == 200) {
+            dapopolare = document.querySelectorAll(".messa-div")[imessa].querySelectorAll(".momento-div")[jcanto];
+            dapopolare.querySelector(".testo-canto").innerHTML = xhr.response;
+        }
+    };
+    xhr.send();
+}
+
