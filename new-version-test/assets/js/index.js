b18ef946194d448b77feaf23702f9ab7531ebbc7
Giulia Sellitto <giuliasellitto.gs@gmail.com>
2024-02-24 12:20:54 +0100

    new version ok

--- new-version-test/assets/js/index.js
+++ new-version-test/assets/js/index.js
@@ -10,63 +10,69 @@ function mostraCantiDelGiorno() {
     fetch("https://coroparrocchiacastelsangiorgio.github.io/db/storico-messe/" + dataMessa + ".json")
         .then(responseMessa => responseMessa.text())
         .then(datiMessa => {
+            let elencoMesse = JSON.parse(datiMessa);
             messaTrovata = true;
-            try {
-                let elencoMesse = JSON.parse(datiMessa);
-                let sectionMessa = document.querySelector("#faq-section");
-                // se ci sono più messe in un giorno, clona il container
-                for (let i = 1; i < elencoMesse.length; i++) {
-                    let container = sectionMessa.querySelector(".container");
-                    let nuovoContainer = container.cloneNode(true);
-                    sectionMessa.appendChild(nuovoContainer);
+            let sectionMessa = document.querySelector("#faq-section");
+            // se ci sono più messe in un giorno, clona il container
+            for (let i = 1; i < elencoMesse.length; i++) {
+                let container = sectionMessa.querySelector(".container");
+                let nuovoContainer = container.cloneNode(true);
+                sectionMessa.appendChild(nuovoContainer);
+            }
+            // carica dati della messa
+            for (let i = 0; i < elencoMesse.length; i++) {
+                let container = sectionMessa.querySelectorAll(".container")[i];
+                let messa = elencoMesse[i];
+                container.querySelector(".data-messa").textContent = messa.datamessa;
+                container.querySelector(".nome-messa").textContent = messa.nome;
+                //container.querySelector(".sottotitolo-messa").textContent = messa.sottotitolo;
+                // popola momenti e canti
+                let accordion = container.querySelector(".accordion");
+                accordion.id = "faqlist-" + i;
+                let accordionItem = accordion.querySelector("#canto-container-i-j");
+                // aggiungi tanti accordion-item quanti sono i canti
+                for (let j = 0; j < messa.scaletta.length; j++) {
+                    let nuovoAccordionItem = accordionItem.cloneNode(true);
+                    nuovoAccordionItem.id = "canto-container-" + i + "-" + j;
+                    nuovoAccordionItem.querySelector(".accordion-button").setAttribute("data-bs-target", "#faq-content-"+ i + "-" + j);
+                    nuovoAccordionItem.querySelector(".num").id ="num" + i + "-" + j;
+                    nuovoAccordionItem.querySelector(".tit").id ="tit" + i + "-" + j;
+                    nuovoAccordionItem.querySelector(".accordion-collapse").id = "faq-content-" + i + "-" + j;
+                    nuovoAccordionItem.querySelector(".accordion-collapse").setAttribute("data-bs-parent", "#faqlist-"+i);
+                    accordion.appendChild(nuovoAccordionItem);
                 }
-                // carica dati della messa
-                for (let i = 0; i < elencoMesse.length; i++) {
-                    let container = sectionMessa.querySelectorAll(".container")[i];
-                    let messa = elencoMesse[i];
-                    container.querySelector(".data-messa").textContent = messa.datamessa;
-                    container.querySelector(".nome-messa").textContent = messa.nome;
-                    //container.querySelector(".sottotitolo-messa").textContent = messa.sottotitolo;
-                    // popola momenti e canti
-                    let accordion = container.querySelector(".accordion");
-                    accordion.id = "faqlist-" + i;
-                    let accordionItem = accordion.querySelector("#canto-container-0");
-                    // aggiungi tanti accordion-item quanti sono i canti
-                    for (let j = 1; j < messa.scaletta.length; j++) {
-                        let nuovoAccordionItem = accordionItem.cloneNode(true);
-                        nuovoAccordionItem.id = "canto-container-" + j;
-                        nuovoAccordionItem.querySelector(".accordion-button").setAttribute("data-bs-target", "#faq-content-"+j);
-                        nuovoAccordionItem.querySelector(".num").id ="num"+j;
-                        nuovoAccordionItem.querySelector(".tit").id ="tit"+j;
-                        nuovoAccordionItem.querySelector(".accordion-collapse").id = "faq-content-"+j;
-                        nuovoAccordionItem.querySelector(".accordion-collapse").setAttribute("data-bs-parent", "#faqlist-"+i);
-                        accordion.appendChild(nuovoAccordionItem);
+                // elimina il canto container usato come base per la clonazione
+                accordion.removeChild(accordionItem);
+                // popola canti
+                for (let j = 0; j < messa.scaletta.length; j++) {
+                    let datiMomento = messa.scaletta[j];
+                    let accordionMomento = accordion.querySelectorAll(".accordion-item")[j];
+                    accordionMomento.querySelector(".num").textContent = datiMomento.momento;
+                    if ("idcanto" in datiMomento) {
+                        popolaTitoloCanto(i, j, datiMomento.idcanto);
+                        popolaTestoCanto(i, j, datiMomento.idcanto);
                     }
-                    // popola canti
-                    for (let j = 0; j < messa.scaletta.length; j++) {
-                        let datiMomento = messa.scaletta[j];
-                        let accordionMomento = accordion.querySelectorAll(".accordion-item")[j];
-                        accordionMomento.querySelector(".num").textContent = datiMomento.momento;
-                        if ("idcanto" in datiMomento) {
-                            popolaTitoloCanto(i, j, datiMomento.idcanto);
-                            popolaTestoCanto(i, j, datiMomento.idcanto);
-                        }
-                        if ("testocanto" in datiMomento) {
-                            accordionMomento.querySelector(".tit").textContent = datiMomento.testocanto;
-                            bottoneDaDisabilitare = accordionMomento.querySelector(".accordion-button");
-                            bottoneDaDisabilitare.removeAttribute("type");
-                            bottoneDaDisabilitare.removeAttribute("data-bs-toggle");
-                            bottoneDaDisabilitare.removeAttribute("data-bs-target");
-                            bottoneDaDisabilitare.style.pointerEvents = "none";
-                        }
+                    if ("testocanto" in datiMomento) {
+                        accordionMomento.querySelector(".tit").textContent = datiMomento.testocanto;
+                        bottoneDaDisabilitare = accordionMomento.querySelector(".accordion-button");
+                        bottoneDaDisabilitare.removeAttribute("type");
+                        bottoneDaDisabilitare.removeAttribute("data-bs-toggle");
+                        bottoneDaDisabilitare.removeAttribute("data-bs-target");
+                        bottoneDaDisabilitare.style.pointerEvents = "none";
                     }
                 }
             }
-            catch (err) {
-                if (!messaTrovata) {
-                    // TODO informare l'utente che non ci sono messe caricate
-                    console.log("Messa non trovata per data: " + dataMessa);
-                }
+        })
+        .catch((error) => {
+            if (!messaTrovata) {
+                console.log("Messa non trovata per data: " + dataMessa);
+                let accordionMomento = document.querySelector(".accordion-item");
+                accordionMomento.querySelector(".tit").textContent = "Non sono stati aggiunti canti";
+                bottoneDaDisabilitare = accordionMomento.querySelector(".accordion-button");
+                bottoneDaDisabilitare.removeAttribute("type");
+                bottoneDaDisabilitare.removeAttribute("data-bs-toggle");
+                bottoneDaDisabilitare.removeAttribute("data-bs-target");
+                bottoneDaDisabilitare.style.pointerEvents = "none";
             }
         });
 }
